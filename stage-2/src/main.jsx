cc66560915193d726968834c9acb15d39d82d47d
skeizable <kibet.koskei@proton.me>
2023-09-14 02:34:03 +0300

    switch to browserrouter

--- stage-2/src/main.jsx
+++ stage-2/src/main.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes, Outlet
 } from "react-router-dom";
 import './index.css'
 import LandingPage from './pages/landing-page.jsx';
 import ErrorPage from './pages/error-page.jsx';
 import MovieDetailsPage from './pages/movie-details.-page';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/movies",
-    element: <MovieDetailsPage />,
-    errorElement: <ErrorPage />
-  },
-]);
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<LandingPage />}>
+          <Route index element={<LandingPage />} />
+
+
+          <Route path="*" element={<ErrorPage />} />
+
+        </Route>
+        <Route path="movies/:movieId" element={<MovieDetailsPage />} />
+
+
+        <Route>
+
+        </Route>
+
+      </Routes>
+
+
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
